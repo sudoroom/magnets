99b46d0676e6043e42cab4904d2a7f98c5c66081
yar <yardenack@gmail.com>
2013-12-24 21:51:10 -0800

    more top layers, sparser support - the top is smoother on most of the surface, so that's good - but the magnet hole still is a dense mess that no magnet would fit into. this is the exact same model as before. further slic3r tweaking ahead...

--- heart/slic3r.sh
+++ heart/slic3r.sh
@@ -3,8 +3,11 @@
 slic3r \
 	 --nozzle-diameter 0.35 \
 	 --layer-height 0.25 \
+	 --top-solid-layers 6 \
 	 --filament-diameter 1.75 \
 	 --temperature 220 \
 	 --print-center 150,150 \
 	 --support-material \
+	 --support-material-pattern rectilinear \
+	 --support-material-spacing 2.5 \
 	 -o heart.gcode heart.stl
