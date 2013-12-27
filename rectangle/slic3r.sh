138b6c2b1f23861a22e3a54173bd16986529f100
yar <yardenack@gmail.com>
2013-12-26 22:25:35 -0800

    test slicer settings on a smaller rectangle - bumping --support-material-spacing to 5 seems noticeable. next time I should try 15...

---
+++ rectangle/slic3r.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+slic3r \
+	 --nozzle-diameter 0.35 \
+	 --layer-height 0.25 \
+	 --top-solid-layers 6 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 150,150 \
+	 --support-material \
+	 --support-material-pattern rectilinear \
+	 --support-material-spacing 5 \
+	 -o rectangle.gcode rectangle.stl
