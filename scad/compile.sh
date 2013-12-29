3f4ecb54474cdb9db7eba6f555653bad1f9c6667
yar <yardenack@gmail.com>
2013-12-28 16:08:42 -0800

    first stab at openscad workflow! - printing a small rectangular prism to test the positioning, scaling, etc - somewhere the positioning information gets lost, we're not sure where, good to know! - hand-edited tempurature in gcode after compiling - print succeeds but because the object is so small, the skirting doesn't give the plastic enough time to start flowing right

---
+++ scad/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+openscad -o rectangle.stl rectangle.scad
+
+slic3r \
+	 --layer-height 0.25 \
+	 --perimeters 1 \
+	 --top-solid-layers 2 \
+	 --nozzle-diameter 0.35 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 150,150 \
+	 --support-material \
+	 --support-material-pattern rectilinear \
+	 --support-material-spacing 40 \
+	 --support-material-interface-layers 0 \
+	 -o rectangle.gcode rectangle.stl
---
+++ scad/rectangle.scad
@@ -0,0 +1,8 @@
+
+module test()
+{
+   cube([10,15,5]);
+}
+
+test();
+
