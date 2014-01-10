e3b0fcc1ad0d212748f2b2b06c4180cbb0d808e8
yar <yardenack@gmail.com>
2014-01-09 21:34:25 -0800

    start testing upside-down magnet hole again

---
+++ mini/compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# probably only useful for me, sorry
+export PATH=/usr/bin:/usr/bin/core_perl:/usr/bin/vendor_perl
+
+# always execute in THIS dir
+self=$(readlink -e "$0") || exit 1
+self=$(dirname "${self}") || exit 1
+cd "$self"
+
+openscad -o mini.stl mini.scad
+
+slic3r \
+	 --layer-height 0.25 \
+	 --perimeters 2 \
+	 --solid-layers 3 \
+	 --nozzle-diameter 0.35 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 180,50 \
+	 --skirts 3 \
+	 --fill-density 0.4 \
+	 -o mini.gcode mini.stl
+
+sed -i 's/M104 S200/M104 S220/' mini.gcode
+sed -i 's/M109 S200/M109 S220/' mini.gcode
---
+++ mini/mini.scad
@@ -0,0 +1,5 @@
+difference() {
+	translate([-10,-10,0]) cube([20,20,5]);
+	translate([0,0,-0.1]) cylinder(3,5.25,5.25);
+	translate([0,0,.25])  cylinder(3,6.5,6.5);
+}
