3bec93dc79bcd2e6af0f15bc636c6e7d918cb43a
yar <yardenack@gmail.com>
2014-01-10 16:41:15 -0800

    less dense center, but more solid sides

--- heart/compile.sh
+++ heart/compile.sh
@@ -16,13 +16,14 @@ openscad -o ${name}.stl ${name}.scad
 slic3r \
 	 --layer-height 0.25 \
 	 --perimeters 1 \
-	 --solid-layers 3 \
+	 --top-solid-layers 5 \
+	 --bottom-solid-layers 3 \
 	 --nozzle-diameter 0.35 \
 	 --filament-diameter 1.75 \
 	 --temperature 220 \
 	 --print-center 180,50 \
 	 --skirts 3 \
-	 --fill-density 0.4 \
+	 --fill-density 0.2 \
 	 -o ${tmpfile} ${name}.stl
 
 sed -i 's/M104 S200/M104 S220/' ${tmpfile}
