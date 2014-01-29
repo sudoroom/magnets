d064f2d2544664eb54c42fe2fecb9ed4bb4d32b4
yar <yardenack@gmail.com>
2014-01-29 14:43:23 -0800

    test shape to see how a dome works

---
+++ compile-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# probably only useful for me, sorry
+export PATH=/usr/bin:/usr/bin/core_perl:/usr/bin/vendor_perl
+
+# always execute in THIS dir
+self=$(readlink -e "$0") || exit 1
+self=$(dirname "${self}") || exit 1
+cd "$self"
+
+name=test
+tmpfile=/tmp/.magnet.$EUID.$$.$(date +%s).$RANDOM
+
+openscad -o ${name}.stl ${name}.scad
+
+slic3r \
+	 --layer-height 0.25 \
+	 --perimeters 1 \
+	 --solid-layers 5 \
+	 --nozzle-diameter 0.35 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 100,100 \
+	 --skirts 5 \
+	 --fill-density 0.12 \
+	 -o ${tmpfile} ${name}.stl
+
+sed -i 's/M104 S200/M104 S220/' ${tmpfile}
+sed -i 's/M109 S200/M109 S220/' ${tmpfile}
+
+awk '/^G1 Z2.35/ {print "G1 Z60.000\nG4 P12000"}
+	{print}' ${tmpfile} >| ${name}.gcode
+
+rm -f ${tmpfile}
---
+++ test.scad
@@ -0,0 +1,12 @@
+translate([0,0,1.25]) difference() {
+	rotate([90,0,0]) cylinder(30,15,15,$fn=200);
+	rotate([90,0,0]) translate([0,0,0.25])
+		cylinder(29.5,14.5,14.5,$fn=200);
+	translate([-16,-31,-16]) cube([32,32,16]);
+}
+difference() {
+	linear_extrude(height=1.25)
+		translate([-15,-30])
+		square([30,30]);
+	translate([0,-15,-0.1]) cylinder(1.75,0.5,0.5);
+}
