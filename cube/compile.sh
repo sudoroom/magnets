e8655c74870c1ae9090ff28116bfce98055b80eb
yar <yardenack@gmail.com>
2014-01-31 12:13:02 -0800

    [cube] try perspective-flattening a cube

---
+++ cube/compile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# probably only useful for me, sorry
+export PATH=/usr/bin:/usr/bin/core_perl:/usr/bin/vendor_perl
+
+# always execute in THIS dir
+self=$(readlink -e "$0") || exit 1
+self=$(dirname "${self}") || exit 1
+cd "$self"
+
+name=cube
+tmpfile=/tmp/.magnet.$EUID.$$.$(date +%s).$RANDOM
+
+openscad -o ${name}.stl ${name}.scad
+
+slic3r \
+	 --layer-height 0.25 \
+	 --perimeters 1 \
+	 --top-solid-layers 5 \
+	 --bottom-solid-layers 3 \
+	 --nozzle-diameter 0.35 \
+	 --filament-diameter 1.75 \
+	 --temperature 220 \
+	 --print-center 70,50 \
+	 --skirts 2 \
+	 --fill-density 0.15 \
+	 -o ${tmpfile} ${name}.stl
+
+sed -i 's/M104 S200/M104 S220/' ${tmpfile}
+sed -i 's/M109 S200/M109 S220/' ${tmpfile}
+
+awk '/^G1 Z2.35/ {print "G1 Z6.000\nG4 P10000"}
+	{print}' ${tmpfile} >| ${name}.gcode
+
+rm -f ${tmpfile}
